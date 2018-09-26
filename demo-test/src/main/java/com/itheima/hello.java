971676526f96b28d7b6abc19095f457985fbc2c7
xjj <1425443710@qq.com>
2018-09-26 20:45:02 +0800

    版本1

---
+++ demo-test/src/main/java/com/itheima/hello.java
@@ -0,0 +1,8 @@
+package com.itheima;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("-----111-----");
+        System.out.println("-----222-----");
+    }
+}
